fc5217066de5fd5a3049bcef1cbc7b2f19d53107
adommaker-blip <enilcarocastro@gmail.com>
2026-05-20 02:22:14 -0400

    Update quickstart.py

--- quickstart.py
+++ quickstart.py
@@ -3,7 +3,7 @@ from instapy import InstaPy
 from instapy import smart_run
 
 # login credentials
-insta_username = ''
+insta_username = 'st4r_josue67'
 insta_password = ''
 
 comments = ['Nice shot! @{}',
